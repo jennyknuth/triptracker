e67d381491449cf8e5da583224af3347fea930a1
Jenny Knuth <inkboco@gmail.com>
2015-07-18 19:18:53 -0600

    parent can register, form validated

--- routes/auth.js
+++ routes/auth.js
@@ -47,5 +47,37 @@ router.post('/login', function (req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register');
 });
-
+router.post('/register', function (req, res, next) {
+  errors = []
+  if (!req.body.password) {
+    errors.push("Password can't be blank")
+  }
+  if (!req.body.email) {
+    errors.push("Email can't be blank")
+  }
+  if (errors.length) {
+    console.log(req.body);
+    res.render('register', {
+      errors: errors,
+      content: req.body
+    })
+  } else {
+    users.find({email: req.body.email}, function (err, docs) {
+      if (docs.length === 0) {
+        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+        users.insert(req.body, function (err, doc) {
+          if (err) {
+            res.redirect('/register')
+          }
+          // req.session.id = doc._id
+          req.session.email = req.body.email
+          res.redirect('users/index')
+        })
+      } else {
+        errors.push('Login invalid')
+        res.render('register', {errors: errors})
+      }
+    })
+  }
+});
 module.exports = router;
