411e8ce4583ac5ed43358381fc5bed1c2214861a
Jenny Knuth <inkboco@gmail.com>
2015-09-12 17:26:23 -0600

    basic back-end files set up

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').load();
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var users = require('./routes/users');
+var trips = require('./routes/trips');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+
+app.use('/api/users', users);
+app.use('/api/trips', trips);
+
+app.listen(process.env.PORT || 8080);
+console.log('Woot, up and running!')
+
+module.exports = app;
