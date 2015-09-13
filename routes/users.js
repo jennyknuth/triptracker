ac85f14d51af353291924e0a086f5afefd01e6a0
Jenny Knuth <inkboco@gmail.com>
2015-09-13 11:57:56 -0600

    added user routes

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,20 @@ var db = require('monk')(process.env.MONGOLAB_URI);
 var trips = db.get('trips');
 var users = db.get('users');
 
+router.get('/', function (req, res) {
+  users.find({}, function (err, users) {
+    if (err) res.send(err);
+    res.status(200).json(users);
+  });
+});
+
+router.post('/', function (req, res) {
+  users.insert(req.body, function (err, user) {
+    if (err) res.send(err);
+    res.status(200).json(user);
+  });
+});
+
 router.get('/:id', function (req, res) {
   trips.find({userId: req.params.id}, function (err, trips) {
     if (err) res.send(err);
